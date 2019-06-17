58fc8556f8a3409ab8b61b3b414500de40b557fb
fuguanghe <fgh_email@163.com>
2019-06-17 17:14:53 +0800

    1，2，3，4季已学习

---
+++ lib.js
@@ -0,0 +1,129 @@
+let baseUrl = "/example";
+let routeConfig = [
+    {
+        "title":"内部指令",
+        "menu":[
+            {
+                "name":"if-else-show",
+                "url":baseUrl+"/basics/v-if-else-show.html"
+            },
+            {
+                "name":"for",
+                "url":baseUrl+"/basics/v-for.html"
+            },
+            {
+                "name":"text",
+                "url":baseUrl+"/basics/v-text.html"
+            },
+            {
+                "name":"on",
+                "url":baseUrl+"/basics/v-on.html"
+            },
+            {
+                "name":"model",
+                "url":baseUrl+"/basics/v-model.html"
+            },
+            {
+                "name":"bind",
+                "url":baseUrl+"/basics/v-bind.html"
+            },
+            {
+                "name":"ohters",
+                "url":baseUrl+"/basics/ohters.html"
+            },
+        ]
+    },
+    {
+        "title":"全局指令",
+        "menu":[
+            {
+                "name":"directive自定义指令",
+                "url":baseUrl+"/globalApi/directive.html"
+            },
+            {
+                "name":"extend扩展实列构造器",
+                "url":baseUrl+"/globalApi/extend.html"
+            },
+            {
+                "name":"set全局操作",
+                "url":baseUrl+"/globalApi/set.html"
+            },
+            {
+                "name":"生命周期",
+                "url":baseUrl+"/globalApi/lifecycle.html"
+            },
+            {
+                "name":"制作模板",
+                "url":baseUrl+"/globalApi/template.html"
+            },
+            {
+                "name":"Component 组件初时",
+                "url":baseUrl+"/globalApi/Component1.html"
+            },
+            {
+                "name":"component props属性",
+                "url":baseUrl+"/globalApi/component2.html"
+            },
+            {
+                "name":"component 父子组件关系",
+                "url":baseUrl+"/globalApi/component3.html"
+            },
+            {
+                "name":"component 标签",
+                "url":baseUrl+"/globalApi/component4.html"
+            }
+
+        ]
+    },
+    {
+        "title":"选项",
+        "menu":[
+            {
+                "name":"propsData Option 全局扩展的数据传递",
+                "url":baseUrl+"/option/propsData.html"
+            },
+            {
+                "name":"computed Option 计算选项",
+                "url":baseUrl+"/option/computed.html"
+            },
+            {
+                "name":"Methods Option 方法选项",
+                "url":baseUrl+"/option/methods.html"
+            },
+            {
+                "name":"Watch 选项 监控数据",
+                "url":baseUrl+"/option/watch.html"
+            },
+            {
+                "name":"Mixins 混入选项操作",
+                "url":baseUrl+"/option/mixins.html"
+            },
+            {
+                "name":"Extends Option 扩展选项",
+                "url":baseUrl+"/option/extends.html"
+            }
+        ]
+    },
+    {
+        "title":"实力和内置组件",
+        "menu":[
+            {
+                "name":"实例属性",
+                "url":baseUrl+"/sample/attr.html"
+            },
+            {
+                "name":"实例方法",
+                "url":baseUrl+"/sample/func.html"
+            },
+            {
+                "name":"实例事件",
+                "url":baseUrl+"/sample/event.html"
+            },
+            {
+                "name":"内置组件 -slot",
+                "url":baseUrl+"/sample/slot.html"
+            }
+        ]
+    }
+]
+export{routeConfig}
